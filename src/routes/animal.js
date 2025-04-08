2222c123948de32ace3e64bed91377d83c581df8
cesar <cesarjavierveloza501@gmail.com>
2025-04-08 16:25:44 -0500

    cambios

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -9,12 +10,14 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+/*
 //Buscar animal
 router.get("/animals", (req, res) => {
     animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+*/
 //Consultar un animal por su id
 router.get("/animals/:id", (req, res) => {
     const { id } = req.params;
@@ -34,5 +37,22 @@ router.put("/animals/:id", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-
+//Eliminar un animal por su id
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+//consultar todos los animales
+router.get("/animals", verifyToken, (req, res) => {
+    animalSchema.find()
+        .then((data) => res.jason(data))
+        .catch((error) => res.jason({ message: error }));
+});
 module.exports = router;
